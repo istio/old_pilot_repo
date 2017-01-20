e222a35be325999b3bf8092984f5a0ae9bcd58b9
Kuat Yessenov <kuat@google.com>
2017-01-20 11:38:19 -0800

    lint everything

--- bin/init.sh
+++ bin/init.sh
@@ -6,3 +6,7 @@ bin/bazel_to_go.py > /dev/null
 
 # Remove doubly-vendorized k8s dependencies
 rm -rf vendor/k8s.io/client-go/vendor
+
+go install ./...
+go get -u github.com/alecthomas/gometalinter
+gometalinter --install
