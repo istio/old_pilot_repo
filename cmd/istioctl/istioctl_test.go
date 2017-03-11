f308e4ebb682e820fd0fadf8ce5024dc7849b88e
Ed Snible <snible@us.ibm.com>
2017-03-11 09:17:17 -0500

    Attempt to get testdata yaml recognized by Bazel

--- cmd/istioctl/istioctl_test.go
+++ cmd/istioctl/istioctl_test.go
@@ -15,7 +15,6 @@
 package main
 
 import (
-	"os"
 	"testing"
 
 	"istio.io/manager/cmd"
@@ -43,9 +42,6 @@ func TestInvalidRuleStructure(t *testing.T) {
 }
 
 func TestCreateReplaceDeleteRoutes(t *testing.T) {
-	dir, _ := os.Getwd()
-	t.Fatalf("Before setting up root command current directory is: %v", dir)
-
 	file = "testdata/four-route-rules.yaml"
 	if err := cmd.RootCmd.PersistentPreRunE(postCmd, []string{}); err != nil { // Set up Client
 		t.Fatalf("Could not set up root command: %v", err)
