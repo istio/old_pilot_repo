3543d8b6cafd292b94934c55b43b136acd1cebf0
Kuat Yessenov <kuat@google.com>
2017-04-12 12:52:47 -0700

    Separate discovery and apiserver

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -44,6 +44,7 @@ type args struct {
 	passthrough   []int
 	apiserverPort int
 
+	// ingress sync mode is set to off by default
 	controllerOptions kube.ControllerOptions
 	discoveryOptions  envoy.DiscoveryServiceOptions
 
@@ -95,30 +96,36 @@ var (
 		Use:   "discovery",
 		Short: "Start Istio Manager discovery service",
 		RunE: func(c *cobra.Command, args []string) (err error) {
-			flags.controllerOptions.IngressSyncMode = kube.IngressOff
 			controller := kube.NewController(client, flags.controllerOptions)
 			context := &proxy.Context{
-				Discovery: controller,
-				Accounts:  controller,
-				Config: &model.IstioRegistry{
-					ConfigRegistry: controller,
-				},
+				Discovery:  controller,
+				Accounts:   controller,
+				Config:     &model.IstioRegistry{ConfigRegistry: controller},
 				MeshConfig: mesh,
 			}
 			discovery, err := envoy.NewDiscoveryService(controller, context, flags.discoveryOptions)
 			if err != nil {
 				return fmt.Errorf("failed to create discovery service: %v", err)
 			}
-			apiserver := apiserver.NewAPI(apiserver.APIServiceOptions{
-				Version: "v1alpha1",
-				Port:    flags.apiserverPort,
-				Registry: &model.IstioRegistry{
-					ConfigRegistry: controller,
-				},
-			})
 			stop := make(chan struct{})
 			go controller.Run(stop)
 			go discovery.Run()
+			cmd.WaitSignal(stop)
+			return
+		},
+	}
+
+	apiserverCmd = &cobra.Command{
+		Use:   "apiserver",
+		Short: "Start Istio Manager config API service",
+		RunE: func(*cobra.Command, []string) (err error) {
+			controller := kube.NewController(client, flags.controllerOptions)
+			apiserver := apiserver.NewAPI(apiserver.APIServiceOptions{
+				Version:  "v1alpha1",
+				Port:     flags.apiserverPort,
+				Registry: &model.IstioRegistry{ConfigRegistry: controller},
+			})
+			stop := make(chan struct{})
 			go apiserver.Run()
 			cmd.WaitSignal(stop)
 			return
@@ -134,7 +141,6 @@ var (
 		Use:   "sidecar",
 		Short: "Istio Proxy sidecar agent",
 		RunE: func(c *cobra.Command, args []string) (err error) {
-			flags.controllerOptions.IngressSyncMode = kube.IngressOff
 			controller := kube.NewController(client, flags.controllerOptions)
 			context := &proxy.Context{
 				Discovery:        controller,
@@ -210,8 +216,8 @@ func init() {
 	discoveryCmd.PersistentFlags().BoolVar(&flags.discoveryOptions.EnableCaching, "discovery_cache", true,
 		"Enable caching discovery service responses")
 
-	discoveryCmd.PersistentFlags().IntVar(&flags.apiserverPort, "apiPort", 8081,
-		"API service port")
+	apiserverCmd.PersistentFlags().IntVar(&flags.apiserverPort, "port", 8081,
+		"Config API service port")
 
 	proxyCmd.PersistentFlags().StringVar(&flags.ipAddress, "ipAddress", "",
 		"IP address. If not provided uses ${POD_IP} environment variable.")
@@ -237,6 +243,7 @@ func init() {
 	cmd.AddFlags(rootCmd)
 
 	rootCmd.AddCommand(discoveryCmd)
+	rootCmd.AddCommand(apiserverCmd)
 	rootCmd.AddCommand(proxyCmd)
 	rootCmd.AddCommand(version.VersionCmd)
 }
