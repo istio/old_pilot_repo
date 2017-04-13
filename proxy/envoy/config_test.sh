7b29ec9b791397ead9fcbaca4b05368ec88dece5
Kuat Yessenov <kuat@google.com>
2017-04-13 15:08:30 -0700

    add a config test

---
+++ proxy/envoy/config_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Check that envoy can parse golden configuration artifacts
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+proxy/envoy/envoy -l trace -c proxy/envoy/testdata/envoy-v0.json.golden --service-cluster istio-proxy --service-node 10.0.0.1
