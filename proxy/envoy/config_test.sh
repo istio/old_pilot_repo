7aa98bc8eae438f617d10c5b36e9154398f3353e
Kuat Yessenov <kuat@google.com>
2017-04-13 18:18:25 -0700

    making the sandbox work

--- proxy/envoy/config_test.sh
+++ proxy/envoy/config_test.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 # Check that envoy can parse golden configuration artifacts
+# In the future, we can run functional tests, but all configs require iptables rules to be effective.
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
-#proxy/envoy/envoy -l trace -c proxy/envoy/testdata/envoy-v0.json.golden --service-cluster istio-proxy --service-node 10.0.0.1
+ENVOY="proxy/envoy/envoy -l trace --service-cluster istio-proxy --service-node 10.0.0.1"
+CONFIG_FILES="envoy-v0 envoy-fault ingress-envoy"
+
+# TODO the test skips over SSL configuration files since referenced cert files are not in the right places
+
+ls /dev/shm/
+touch /dev/shm/test
+
+for f in ${CONFIG_FILES}; do
+  echo "##############################"
+  echo "# Testing $f..."
+  echo "##############################"
+  ${ENVOY} -c proxy/envoy/testdata/$f.json.golden &
+  PID=$!
+  sleep 1
+  kill -KILL $PID
+done
