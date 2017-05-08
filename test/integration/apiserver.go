e866f603969dace75a9ebe749840def3cca7e1b5
Ed Snible <snible@us.ibm.com>
2017-05-07 22:19:59 -0400

    Retry if unexpected codes and data arrive

--- test/integration/apiserver.go
+++ test/integration/apiserver.go
@@ -46,6 +46,12 @@ type httpRequest struct {
 	data                 interface{}
 	expectedResponseCode int
 	expectedBody         interface{}
+
+	// These HTTP methods are not correct, but allowed before the rules database becomes consistent
+	retryOn map[int]bool
+
+	// These response values are not correct, but allowed before the rules database becomes consistent
+	retryIfMatch []interface{}
 }
 
 var (
@@ -93,6 +99,9 @@ var (
 			}},
 		},
 	}
+
+	// The URL we will use for talking directly to apiserver
+	testURL string
 )
 
 func (r *apiServerTest) String() string {
@@ -130,6 +139,8 @@ func (r *apiServerTest) setup() error {
 		time.Sleep(1 * time.Second)
 	}
 
+	testURL = "http://localhost:8081/v1alpha1/config/route-rule/" + r.Namespace + "/reviews-default"
+
 	return nil
 }
 
@@ -140,66 +151,107 @@ func (r *apiServerTest) teardown() {
 }
 
 func (r *apiServerTest) run() error {
-	return r.routeRuleCRUD()
+	if err := r.routeRuleInvalidDetected(); err != nil {
+		return err
+	}
+	if err := r.routeRuleCRUD(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *apiServerTest) routeRuleInvalidDetected() error {
+	httpSequence := []httpRequest{
+		// Can't create invalid rules
+		{
+			method: "POST", url: "http://localhost:8081/v1alpha1/config/route-rule/" + r.Namespace + "/reviews-invalid",
+			data:                 jsonInvalidRule,
+			expectedResponseCode: net_http.StatusInternalServerError,
+		},
+	}
+
+	return verifySequence(httpSequence)
 }
 
 // routeRuleCRUD attempts to talk to the apiserver to Create, Retrieve, Update, and Delete a rule
 func (r *apiServerTest) routeRuleCRUD() error {
-	client := &net_http.Client{}
 
+	// Run through the lifecycle of updating a rule, with errors, to verify the sequence works as expected.
 	httpSequence := []httpRequest{
-		// Can't get before created
+		// Step 0 Can't get before created
 		{
-			"GET", "http://localhost:8081/v1alpha1/config/route-rule/" + r.Namespace + "/reviews-default", "",
-			net_http.StatusNotFound, nil,
+			method: "GET", url: testURL,
+			expectedResponseCode: net_http.StatusNotFound,
 		},
-		// Can create
+		// Step 1 Can create
 		{
-			"POST", "http://localhost:8081/v1alpha1/config/route-rule/" + r.Namespace + "/reviews-default", jsonRule,
-			net_http.StatusCreated, jsonRule,
+			method: "POST", url: testURL,
+			data:                 jsonRule,
+			expectedResponseCode: net_http.StatusCreated,
+			expectedBody:         jsonRule,
+			// @@ retryOn:              map[int]bool{net_http.StatusInternalServerError: true},
 		},
-		// Can't create twice
+		// Step 2 Can't create twice
 		{
 			// TODO should be StatusForbidden but the server is returning 500 (incorrectly?)
-			"POST", "http://localhost:8081/v1alpha1/config/route-rule/" + r.Namespace + "/reviews-default", jsonRule,
-			net_http.StatusInternalServerError, nil,
+			method: "POST", url: testURL,
+			data:                 jsonRule,
+			expectedResponseCode: net_http.StatusInternalServerError,
 		},
-		// Can't create invalid rules
+		// Step 3 Can get
 		{
-			// TODO should be net_http.StatusForbidden but the server is returning 500 (incorrectly?)
-			"POST", "http://localhost:8081/v1alpha1/config/route-rule/" + r.Namespace + "/reviews-invalid", jsonInvalidRule,
-			net_http.StatusInternalServerError, nil,
+			method: "GET", url: testURL,
+			expectedResponseCode: net_http.StatusOK,
+			expectedBody:         jsonRule,
+			retryOn:              map[int]bool{net_http.StatusNotFound: true},
 		},
-		// Can get
+		// Step 4: Can update
 		{
-			"GET", "http://localhost:8081/v1alpha1/config/route-rule/" + r.Namespace + "/reviews-default", "",
-			net_http.StatusOK, jsonRule,
-		},
-		// Can update
-		{
-			"PUT", "http://localhost:8081/v1alpha1/config/route-rule/" + r.Namespace + "/reviews-default", jsonRule2,
-			net_http.StatusOK, jsonRule2,
+			method: "PUT", url: testURL,
+			data:                 jsonRule2,
+			expectedResponseCode: net_http.StatusOK,
+			expectedBody:         jsonRule2,
 		},
 		// Can still GET after update
 		{
-			"GET", "http://localhost:8081/v1alpha1/config/route-rule/" + r.Namespace + "/reviews-default", "",
-			net_http.StatusOK, jsonRule2,
+			method: "GET", url: testURL,
+			expectedResponseCode: net_http.StatusOK,
+			expectedBody:         jsonRule2,
+			retryOn:              map[int]bool{net_http.StatusNotFound: true},
+			retryIfMatch:         []interface{}{jsonRule},
 		},
 		// Can delete
 		{
-			// Should (perhaps?) be StatusNoContent
-			"DELETE", "http://localhost:8081/v1alpha1/config/route-rule/" + r.Namespace + "/reviews-default", "",
-			net_http.StatusOK, nil,
+			method: "DELETE", url: testURL,
+			expectedResponseCode: net_http.StatusOK, // Should (perhaps?) be StatusNoContent
+			retryOn:              map[int]bool{net_http.StatusNotFound: true},
 		},
 		// Can't delete twice
 		{
-			// Should be StatusNotFound
-			"DELETE", "http://localhost:8081/v1alpha1/config/route-rule/" + r.Namespace + "/reviews-default", "",
-			net_http.StatusInternalServerError, nil,
+			method: "DELETE", url: testURL,
+			expectedResponseCode: net_http.StatusInternalServerError, // Should be StatusNotFound
 		},
 	}
 
-	for _, hreq := range httpSequence {
+	err := verifySequence(httpSequence)
+
+	// On error, clean up any rule we created that didn't get deleted by the final requests
+	if err != nil {
+		// Attempt to delete rule, in case we created this and left it around due to a failure.
+		client := &net_http.Client{}
+		if req, err2 := net_http.NewRequest("DELETE", testURL, nil); err2 != nil {
+			client.Do(req) // #nosec
+		}
+	}
+
+	return err
+}
+
+// Verify a sequence of HTTP requests produce the expected responses
+func verifySequence(httpSequence []httpRequest) error {
+
+	for rulenum, hreq := range httpSequence {
+
 		var err error
 		var bytesToSend []byte
 		if hreq.data != nil {
@@ -208,49 +260,100 @@ func (r *apiServerTest) routeRuleCRUD() error {
 				return err
 			}
 		}
-		req, err := net_http.NewRequest(hreq.method, hreq.url, bytes.NewBuffer(bytesToSend))
-		if err != nil {
-			return err
-		}
-		req.Header.Add("Content-Type", "application/json")
-		resp, err := client.Do(req)
-		if err != nil {
-			return err
-		}
-		defer func() { _ = resp.Body.Close() }() // #nosec
-		if resp.StatusCode != hreq.expectedResponseCode {
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				return err
-			}
-			return fmt.Errorf("%v to %q expected %v but got %v %q", hreq.method, hreq.url,
-				hreq.expectedResponseCode, resp.StatusCode, body)
-		}
-		if hreq.expectedBody != nil {
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				return err
-			}
 
-			var jsonBody interface{}
-			if err = json.Unmarshal(body, &jsonBody); err == nil {
-				if !reflect.DeepEqual(jsonBody, hreq.expectedBody) {
-					serializedExpectedBody, _ := json.Marshal(hreq.expectedBody)
-					if err != nil {
-						return err
-					}
-					return fmt.Errorf("%v to %q expected JSON body %v but got %v", hreq.method, hreq.url,
-						string(serializedExpectedBody), string(body))
-				}
-			} else {
-				// The returned data was not JSON, compare anyway
-				if !reflect.DeepEqual(body, hreq.expectedBody) {
-					return fmt.Errorf("%v to %q expected body %v but got %q", hreq.method, hreq.url,
-						hreq.expectedBody, body)
-				}
-			}
+		// Verify a correct response comes back.  Retry up to 5 times in case data is initially incorrect
+		if err = verifyRequest(hreq, bytesToSend, 5, rulenum); err != nil {
+			return err
 		}
 	}
 
 	return nil
+}
+
+// Verify an HTTP requests produces the expected responses or a small number of the expected error responses
+func verifyRequest(hreq httpRequest, bytesToSend []byte, maxRetries int, rulenum int) error {
+	client := &net_http.Client{}
+
+	// Keep track if we need to retry before apiserver becomes consistent
+	retries := 0
+
+	for {
+		req, err := net_http.NewRequest(hreq.method, hreq.url, bytes.NewBuffer(bytesToSend))
+		if err != nil {
+			return err
+		}
+
+		if hreq.method == "POST" || hreq.method == "PUT" {
+			req.Header.Add("Content-Type", "application/json")
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		if err = resp.Body.Close(); err != nil {
+			return err
+		}
+
+		// Did apiserver fail to return the expected response code?
+		if resp.StatusCode != hreq.expectedResponseCode {
+			// Did it return a code that could lead to a correct code after data becomes consistent?
+			if rt, ok := hreq.retryOn[resp.StatusCode]; !ok || !rt || retries > maxRetries {
+				return fmt.Errorf("%v to %q expected %v but got %v %q on step %v after %v attempts", hreq.method, hreq.url,
+					hreq.expectedResponseCode, resp.StatusCode, body, rulenum, retries)
+			}
+
+			// Give the system a moment after data-changing operations
+			time.Sleep(1 * time.Second)
+			fmt.Printf("@@@ ecs retrying %v %v because of status code\n", hreq.method, hreq.url)
+			retries++
+			continue
+		}
+
+		// Response code matches expectation.  We are done if we don't require a particular response body
+		if hreq.expectedBody == nil {
+			return nil
+		}
+
+		var jsonBody interface{}
+		if err = json.Unmarshal(body, &jsonBody); err == nil {
+			if reflect.DeepEqual(jsonBody, hreq.expectedBody) {
+				return nil
+			}
+
+			if retries >= maxRetries {
+				serializedExpectedBody, err := json.Marshal(hreq.expectedBody)
+				if err != nil {
+					return err
+				}
+				return fmt.Errorf("%v to %q expected JSON body %v but got %v on step %v after %v attempts", hreq.method, hreq.url,
+					string(serializedExpectedBody), string(body), rulenum, retries+1)
+			}
+
+			// Does the data match transient data we tolerate while waiting for consistency?
+			for retryIfMatch := range hreq.retryIfMatch {
+				if reflect.DeepEqual(jsonBody, retryIfMatch) {
+					// Give the system a moment after data-changing operations
+					time.Sleep(1 * time.Second)
+					retries++
+					fmt.Printf("@@@ ecs retrying %v %v because of body\n", hreq.method, hreq.url)
+					continue
+				}
+			}
+		} else {
+			// The returned data was not JSON
+			if retries >= maxRetries {
+				return fmt.Errorf("%v to %q expected body %v but got %q on step %v after %v attempts", hreq.method, hreq.url,
+					hreq.expectedBody, body, rulenum, retries+1)
+			}
+
+			retries++
+		}
+
+	} // end for (ever)
 }
