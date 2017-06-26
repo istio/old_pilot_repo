cb4ef42516ec4f708cc1265f08fd9920929bec47
Zijun Wang <zijun.wang@ibm.com>
2017-06-26 12:03:32 -0400

    update changes to test

--- bookinfo_istio/test-scripts/run_test.sh
+++ bookinfo_istio/test-scripts/run_test.sh
@@ -3,7 +3,6 @@ hub="docker.io/kimikowang"
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 WORKSPACE=$SCRIPTDIR/../..
 BINDIR=$WORKSPACE/bazel-bin
-REGDIR=$SCRIPTDIR/../registry
 APPSDIR=$SCRIPTDIR/../apps
 BUILDPATH=$SCRIPTDIR/../docker
 PILOTSRCPATH=$WORKSPACE/cmd/pilot
@@ -20,8 +19,6 @@ fi
 set -x
 set -o errexit
 
-#docker push $hub
-
 # Copy the pilot binary to each app dir
 # Build the images and  push them to hub
 for app in details productpage ratings reviews; do
@@ -29,7 +26,7 @@ for app in details productpage ratings reviews; do
   make -C $APPSDIR/$app build build.dockerize docker.push clean GOOS=linux GOARCH=amd64 HUB=$hub
 done
 
-# Bring up all app containers onto Docker
+# Bring up all app containers
 cd $BUILDPATH
 docker-compose up -d
 STATUS=$?
