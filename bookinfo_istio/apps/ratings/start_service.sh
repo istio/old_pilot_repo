95bbe89739dab53d10fa8b077285e47f235c53e6
Zijun Wang <zijun.wang@ibm.com>
2017-07-28 19:44:55 -0400

    remove demo and istioctl

--- bookinfo_istio/apps/ratings/start_service.sh
+++
@@ -1,5 +0,0 @@
-suffix=$1
-
-/opt/istio/prepare_proxy.sh -p 15001 -u 1337
-/opt/microservices/ratings 9080 &
-su istio -c "/opt/istio/pilot-agent proxy --adapter VMs --vmsconfig /etc/config.yaml.$suffix > /tmp/envoy.log" 
--- bookinfo_istio/apps/reviews/start_service.sh
+++
@@ -1,5 +0,0 @@
-suffix=$1
-
-/opt/istio/prepare_proxy.sh -p 15001 -u 1337
-/opt/microservices/reviews 9080 http://localhost:6379 &
-su istio -c "/opt/istio/pilot-agent proxy --adapter VMs --vmsconfig /etc/config.yaml.$suffix > /tmp/envoy.log" 
