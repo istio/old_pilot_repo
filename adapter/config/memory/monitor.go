c9015635de06bdb74c3ea3b219f560a885cb6085
Zijun Wang <zijun.wang@ibm.com>
2017-08-08 14:21:50 -0400

    update monitor for inmem config store

--- adapter/config/memory/monitor.go
+++ adapter/config/memory/monitor.go
@@ -1,3 +1,17 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package memory
 
 import (
@@ -10,13 +24,11 @@ import (
 	"istio.io/pilot/model"
 )
 
-// Configs TODO
 type Configs []model.Config
 
 // Handler specifies a function to apply on a Config for a given event type
 type Handler func(model.Config, model.Event)
 
-// Monitor TODO
 type Monitor interface {
 	Start(<-chan struct{})
 	AppendEventHandler(string, Handler)
@@ -26,13 +38,17 @@ type configsMonitor struct {
 	store              model.ConfigStore
 	configCachedRecord map[string]Configs
 	handlers           map[string][]Handler
-	period             time.Duration
+
+	ticker   *time.Ticker
+	period   time.Duration
+
+	stop <-chan struct{}
 }
 
-// NewConfigsMonitor TODO
 func NewConfigsMonitor(store model.ConfigStore, period time.Duration) Monitor {
-	cache := make(map[string]Configs)
-	handlers := make(map[string][]Handler)
+	cache := make(map[string]Configs, 0)
+	handlers := make(map[string][]Handler, 0)
+
 	for _, conf := range model.IstioConfigTypes {
 		cache[conf.Type] = make(Configs, 0)
 		handlers[conf.Type] = make([]Handler, 0)
@@ -47,17 +63,16 @@ func NewConfigsMonitor(store model.ConfigStore, period time.Duration) Monitor {
 }
 
 func (m *configsMonitor) Start(stop <-chan struct{}) {
+	m.ticker = time.NewTicker(m.period)
 	m.run(stop)
 }
 
 func (m *configsMonitor) run(stop <-chan struct{}) {
-	ticker := time.NewTicker(m.period)
 	for {
 		select {
 		case <-stop:
-			ticker.Stop()
-			return
-		case <-ticker.C:
+			m.ticker.Stop()
+		case <-m.ticker.C:
 			m.UpdateConfigRecord()
 		}
 	}
@@ -72,23 +87,48 @@ func (m *configsMonitor) UpdateConfigRecord() {
 		}
 		newRecord := Configs(configs)
 		newRecord.normalize()
-		if !reflect.DeepEqual(newRecord, m.configCachedRecord[conf.Type]) {
-			// TODO: Make proper comparison to the cache record
-			obj := model.Config{}
-			var event model.Event
-			for _, f := range m.handlers[conf.Type] {
-				currentHandler := f
-				go func(handler Handler) {
-					handler(obj, event)
-				}(currentHandler)
-			}
-			m.configCachedRecord[conf.Type] = newRecord
+		m.compareToCache(conf.Type, m.configCachedRecord[conf.Type], newRecord)
+		m.configCachedRecord[conf.Type] = newRecord
+	}
+}
+
+func (m *configsMonitor) compareToCache(typ string, oldRec, newRec Configs) {
+	io, in := 0, 0
+	for io < len(oldRec) && in < len(newRec) {
+		if reflect.DeepEqual(oldRec[io], newRec[in]) {
+		} else if oldRec[io].Key == newRec[in].Key {
+			// An update event
+			m.applyHandlers(typ, newRec[in], model.EventUpdate)
+		} else if oldRec[io].Key < newRec[in].Key {
+			// A delete event
+			m.applyHandlers(typ, oldRec[io], model.EventDelete)
+			in--
+		} else {
+			// An add event
+			m.applyHandlers(typ, newRec[in], model.EventAdd)
+			io--
 		}
+		io++
+		in++
+	}
+
+	for ; io < len(oldRec); io++ {
+		m.applyHandlers(typ, oldRec[io], model.EventDelete)
+	}
+
+	for ; in < len(newRec); in++ {
+		m.applyHandlers(typ, newRec[in], model.EventAdd)
 	}
 }
 
 func (m *configsMonitor) AppendEventHandler(typ string, h Handler) {
 	m.handlers[typ] = append(m.handlers[typ], h)
+}
+
+func (m *configsMonitor) applyHandlers(typ string, config model.Config, e model.Event) {
+	for _, f := range m.handlers[typ] {
+		f(config, e)
+	}
 }
 
 func (list Configs) normalize() {
