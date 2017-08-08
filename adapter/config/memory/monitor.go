f971320c97b09f4274139dc76de7d388fda7be57
Zijun Wang <zijun.wang@ibm.com>
2017-08-08 14:16:50 -0400

    update memory config store

--- adapter/config/memory/monitor.go
+++ adapter/config/memory/monitor.go
@@ -1,9 +1,25 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package memory
 
 import (
 	"reflect"
 	"sort"
 	"time"
+
+	"github.com/golang/glog"
 
 	"istio.io/pilot/model"
 )
@@ -12,15 +28,6 @@ type Configs []model.Config
 
 // Handler specifies a function to apply on a Config for a given event type
 type Handler func(model.Config, model.Event)
-
-// AddHandler specifies a function to apply on a newly-added object
-type AddHandler func(interface{})
-
-// UpdateHandler specifies a function to apply on an updated object
-type UpdateHandler func(interface{}, interface{})
-
-// DeleteHandler specifies a function to apply on a deleted object
-type DeleteHandler func(interface{})
 
 type Monitor interface {
 	Start(<-chan struct{})
@@ -32,9 +39,8 @@ type configsMonitor struct {
 	configCachedRecord map[string]Configs
 	handlers           map[string][]Handler
 
-	ticker   time.Ticker
+	ticker   *time.Ticker
 	period   time.Duration
-	tickChan <-chan time.Time
 
 	stop <-chan struct{}
 }
@@ -57,7 +63,7 @@ func NewConfigsMonitor(store model.ConfigStore, period time.Duration) Monitor {
 }
 
 func (m *configsMonitor) Start(stop <-chan struct{}) {
-	m.tickChan = time.NewTicker(m.period).C
+	m.ticker = time.NewTicker(m.period)
 	m.run(stop)
 }
 
@@ -65,8 +71,8 @@ func (m *configsMonitor) run(stop <-chan struct{}) {
 	for {
 		select {
 		case <-stop:
-			m.tickChan.Stop()
-		case <-m.tickChan:
+			m.ticker.Stop()
+		case <-m.ticker.C:
 			m.UpdateConfigRecord()
 		}
 	}
@@ -86,7 +92,7 @@ func (m *configsMonitor) UpdateConfigRecord() {
 	}
 }
 
-func (m *configsMonitor) CompareToCache(typ string, oldRec, newRec Configs) {
+func (m *configsMonitor) compareToCache(typ string, oldRec, newRec Configs) {
 	io, in := 0, 0
 	for io < len(oldRec) && in < len(newRec) {
 		if reflect.DeepEqual(oldRec[io], newRec[in]) {
@@ -107,11 +113,11 @@ func (m *configsMonitor) CompareToCache(typ string, oldRec, newRec Configs) {
 	}
 
 	for ; io < len(oldRec); io++ {
-		applyHandlers(typ, oldRec[io], model.EventDelete)
+		m.applyHandlers(typ, oldRec[io], model.EventDelete)
 	}
 
 	for ; in < len(newRec); in++ {
-		applyHandlers(typ, newRec[in], model.EventAdd)
+		m.applyHandlers(typ, newRec[in], model.EventAdd)
 	}
 }
 
@@ -120,7 +126,7 @@ func (m *configsMonitor) AppendEventHandler(typ string, h Handler) {
 }
 
 func (m *configsMonitor) applyHandlers(typ string, config model.Config, e model.Event) {
-	for f := range m.handlers[typ] {
+	for _, f := range m.handlers[typ] {
 		f(config, e)
 	}
 }
