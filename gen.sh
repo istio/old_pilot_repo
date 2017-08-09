ec7b1b5eb94f537b7087d0f642b246779d774ebe
Costin Manolache <costin@google.com>
2017-08-08 17:01:21 -0700

    Add a small 'gen' script to copy bazel artifacts, and the echo.pb

---
+++ gen.sh
@@ -0,0 +1,2 @@
+cp bazel-genfiles/model/mock_config_gen_test.go model/
+cp -a bazel-genfiles/external/com_github_googleapis_googleapis/google/rpc/code.pb.go bazel-genfiles/external/com_github_googleapis_googleapis/google/rpc/error_details.pb.go bazel-genfiles/external/com_github_googleapis_googleapis/google/rpc/status.pb.go ../../github.com/googleapis/google/rpc
