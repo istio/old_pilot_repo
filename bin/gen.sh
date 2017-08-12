5cbe5d9a4801cc5e4bdd1e8d4a015717c1eb9169
Costin Manolache <costin@google.com>
2017-08-11 17:19:43 -0700

    Remove non-essential files

--- bin/gen.sh
+++
@@ -1,2 +0,0 @@
-cp bazel-genfiles/model/mock_config_gen_test.go model/
-cp -a bazel-genfiles/external/com_github_googleapis_googleapis/google/rpc/code.pb.go bazel-genfiles/external/com_github_googleapis_googleapis/google/rpc/error_details.pb.go bazel-genfiles/external/com_github_googleapis_googleapis/google/rpc/status.pb.go ../../github.com/googleapis/google/rpc
