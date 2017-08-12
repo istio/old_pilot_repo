e471babc4d92ed9dec16b28c530f5fbf527a1054
Andra Cismaru <andracis@google.com>
2017-08-11 19:05:13 -0700

    Enable more tests

--- test/integration/driver.go
+++ test/integration/driver.go
@@ -169,12 +169,12 @@ func runTests(envs ...infra) {
 
 		tests := []test{
 			&http{infra: &istio},
-			//&grpc{infra: &istio},
-			//&tcp{infra: &istio},
+			&grpc{infra: &istio},
+			&tcp{infra: &istio},
 			&ingress{infra: &istio},
 			//&egress{infra: &istio},
-			//&routing{infra: &istio},
-			//&zipkin{infra: &istio},
+			&routing{infra: &istio},
+			&zipkin{infra: &istio},
 		}
 
 		for _, test := range tests {
