51da31b61d3bee8b9683b9cdaf05ef99992b63f7
Shriram Rajagopalan <shriram@us.ibm.com>
2017-08-15 08:54:54 -0400

    Revert "undo mock mgmt ports"
    This reverts commit c3869f29f0033954a9a13700327add29581d902f.

--- test/mock/service.go
+++ test/mock/service.go
@@ -215,16 +215,15 @@ func (sd *ServiceDiscovery) HostInstances(addrs map[string]bool) []*model.Servic
 
 // ManagementPorts implements discovery interface
 func (sd *ServiceDiscovery) ManagementPorts(addr string) model.PortList {
-	// return model.PortList{{
-	// 	Name:     "http",
-	// 	Port:     3333,
-	// 	Protocol: model.ProtocolHTTP,
-	// }, {
-	// 	Name:     "custom",
-	// 	Port:     9999,
-	// 	Protocol: model.ProtocolTCP,
-	// }}
-	return nil
+	return model.PortList{{
+		Name:     "http",
+		Port:     3333,
+		Protocol: model.ProtocolHTTP,
+	}, {
+		Name:     "custom",
+		Port:     9999,
+		Protocol: model.ProtocolTCP,
+	}}
 }
 
 // GetIstioServiceAccounts gets the Istio service accounts for a service hostname.
