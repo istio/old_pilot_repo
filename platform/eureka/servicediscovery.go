2d28c4378e491284c4e82dd24e8fe94f76040325
Shriram Rajagopalan <shriram@us.ibm.com>
2017-08-15 21:13:33 -0400

    compile fix

--- platform/eureka/servicediscovery.go
+++ platform/eureka/servicediscovery.go
@@ -110,6 +110,6 @@ func (sd *serviceDiscovery) HostInstances(addrs map[string]bool) []*model.Servic
 // ManagementPorts retries set of health check ports by instance IP.
 // This does not apply to Eureka service registry, as Eureka does not
 // manage the service instances.
-func (c *Controller) ManagementPorts(addr string) model.PortList {
+func (sd *serviceDiscovery) ManagementPorts(addr string) model.PortList {
 	return nil
 }
