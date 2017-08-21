8b54dbc79e1308aaea63c0e8baed6c4fd3eb40c6
Vadim Eisenberg <vadime@il.ibm.com>
2017-08-21 17:05:57 +0300

    istio/istio#552 added EgressRule to pilot discovery

--- cmd/pilot-discovery/main.go
+++ cmd/pilot-discovery/main.go
@@ -80,6 +80,7 @@ var (
 
 			configClient, err := crd.NewClient(flags.kubeconfig, model.ConfigDescriptor{
 				model.RouteRule,
+				model.EgressRule,
 				model.DestinationPolicy,
 			})
 			if err != nil {
