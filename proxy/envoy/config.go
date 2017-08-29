10695d88daa3a03ac9a9f2852799ce9ee7982169
Vadim Eisenberg <vadime@il.ibm.com>
2017-08-29 17:38:27 +0300

    istio/istio#552 set timeout of the external original_dst_cluster cluster
    to be x10 than in-mesh timeout

--- proxy/envoy/config.go
+++ proxy/envoy/config.go
@@ -559,7 +559,11 @@ func buildExternalTrafficVirtualHostOnPort(rule *proxyconfig.EgressRule, mesh *p
 		externalTrafficCluster.Type = ClusterTypeStrictDNS
 		externalTrafficCluster.Hosts = []Host{{URL: fmt.Sprintf("tcp://%s", mesh.EgressProxyAddress)}}
 	} else {
-		externalTrafficCluster = buildOriginalDSTCluster("orig-dst-cluster"+protocolSuffix, mesh.ConnectTimeout)
+		// heuristically define external timeout to be x10 than the in-mesh timeout
+		//TODO consider to define external connect timeout separately in the mesh configuration
+		timeout := mesh.ConnectTimeout * 10
+
+		externalTrafficCluster = buildOriginalDSTCluster("orig-dst-cluster"+protocolSuffix, timeout)
 		if port.Protocol == model.ProtocolHTTPS {
 			externalTrafficCluster.SSLContext = &SSLContextExternal{}
 		}
