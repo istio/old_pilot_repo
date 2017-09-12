ba70ae5460cb9feb40bbb9c2da5edafc19ff69d5
Ryan Kubiak <rkubiak@google.com>
2017-09-12 15:51:07 -0700

    Add flag to specify GCS path to push-debian Add --output_groups=static to push-debian for consistency with push-docker Update release script to pass GCS path to push-debian Remove unnecessary PROJECT_ID flag from release script

--- bin/push-debian.sh
+++ bin/push-debian.sh
@@ -5,29 +5,34 @@
 # bin/push-debian.sh \
 #   -c opt
 #   -v 0.2.1
+#   -p gs://istio-release/releases/0.2.1/deb
 
 function usage() {
   echo "$0 \
     -c <bazel config to use> \
+    -p <GCS path, e.g. gs://istio-release/releases/0.2.1/deb> \
     -v <istio version number>"
   exit 1
 }
 
-while getopts ":c:v:" arg; do
+while getopts ":c:p:v:" arg; do
   case ${arg} in
-    c) BAZEL_ARGS="--config=${OPTARG}";;
+    c) BAZEL_CONFIG="--config=${OPTARG}";;
+    p) GCS_PATH="${OPTARG}";;
     v) ISTIO_VERSION="${OPTARG}";;
     *) usage;;
   esac
 done
 
-if [ -z "${BAZEL_ARGS}" ] || [ -z "${ISTIO_VERSION}" ]; then
+if [ -z "${BAZEL_CONFIG}" ] || [ -z "${ISTIO_VERSION}" ] || [ -z "${GCS_PATH}" ]; then
   usage
 fi
 
 set -ex
 
-bazel ${BAZEL_STARTUP_ARGS} build ${BAZEL_ARGS} "//tools/deb:istio-agent"
+bazel ${BAZEL_STARTUP_ARGS} build \
+  ${BAZEL_CONFIG} --output_groups=static \
+  "//tools/deb:istio-agent"
 gsutil -m cp -r \
   bazel-bin/tools/deb/istio-agent_${ISTIO_VERSION}_amd64.* \
-  gs://istio-release/releases/${ISTIO_VERSION}/deb
+  ${GCS_PATH}
