090f637691aee0b181a477c285a1d4c7fe070fd9
Jason Young <jasonyoung@google.com>
2017-09-12 11:33:29 -0700

    apply proxy config defaults

--- platform/kube/inject/configmap.go
+++ platform/kube/inject/configmap.go
@@ -48,15 +48,27 @@ func GetMeshConfig(kube kubernetes.Interface, namespace,
 		return nil, nil, fmt.Errorf("missing configuration map key %q", ConfigMapKey)
 	}
 
+	var user proxyconfig.MeshConfig
+	if err = model.ApplyYAML(yaml, &user); err != nil {
+		return nil, nil, multierror.Prefix(err, "failed to convert to proto.")
+	}
+	userProxyConfigYAML, err := model.ToYAML(user.DefaultConfig)
+	if err != nil {
+		return nil, nil, multierror.Prefix(err, "failed to re-encode default proxy config")
+	}
+
 	mesh := proxy.DefaultMeshConfig()
 	if err = model.ApplyYAML(yaml, &mesh); err != nil {
 		return nil, nil, multierror.Prefix(err, "failed to convert to proto.")
 	}
-
+	defaultProxyConfig := proxy.DefaultProxyConfig()
+	mesh.DefaultConfig = &defaultProxyConfig
+	if err = model.ApplyYAML(userProxyConfigYAML, user.DefaultConfig); err != nil {
+		return nil, nil, multierror.Prefix(err, "failed to convert to proto.")
+	}
 	if err = model.ValidateMeshConfig(&mesh); err != nil {
 		return nil, nil, err
 	}
-
 	return config, &mesh, nil
 }
 
