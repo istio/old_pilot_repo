dd0a82b03fca2f5253398876de16268f02154d86
Shriram Rajagopalan <shriram@us.ibm.com>
2017-10-20 11:53:01 -0400

    fix sudo

--- bin/install-bazel.sh
+++ bin/install-bazel.sh
@@ -4,7 +4,8 @@ set -e
 
 # Bazel and related dependencies.
 #apt-get install -y openjdk-8-jdk curl
-echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
-apt-get update
-apt-get install -y bazel
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" >bazel.list
+sudo mv bazel.list /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y bazel
