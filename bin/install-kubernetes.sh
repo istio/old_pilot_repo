3885ab8756b6a19e5c1b951efca9842d90fea7c7
Shriram Rajagopalan <shriram@us.ibm.com>
2017-10-20 12:47:03 -0400

    kubeadm

--- bin/install-kubernetes.sh
+++ bin/install-kubernetes.sh
@@ -18,40 +18,13 @@
 set -x
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# Need to enable mount propagation.  Older Ubuntu doesn't use
-# systemd so need to do it manually
-type -a systemctl > /dev/null
-if [ $? -ne 0 ]; then
-    set -e
-    sudo mkdir -p /var/lib/kubelet
-    sudo mount --bind /var/lib/kubelet /var/lib/kubelet
-    sudo mount --make-shared /var/lib/kubelet
-fi
-
-export K8S_VERSION="v1.7.4"
-export ARCH=amd64
-
-docker run -d \
-    --volume=/:/rootfs:ro \
-    --volume=/sys:/sys:ro \
-    --volume=/var/lib/docker/:/var/lib/docker:rw \
-    --volume=/var/lib/kubelet/:/var/lib/kubelet:rw,shared \
-    --volume=/var/run:/var/run:rw \
-    --net=host \
-    --pid=host \
-    --privileged \
-    --name=kubelet \
-    gcr.io/google_containers/hyperkube-${ARCH}:${K8S_VERSION} \
-    /hyperkube kubelet \
-        --hostname-override=127.0.0.1 \
-        --api-servers=http://localhost:8080 \
-        --config=/etc/kubernetes/manifests \
-        --allow-privileged --v=2
+K8S_VERSION="v1.7.4"
+ARCH="amd64"
 
 # Install kubernetes CLI
-curl -L http://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/${ARCH}/kubectl > /tmp/kubectl
-sudo mv /tmp/kubectl /usr/local/bin
-sudo chmod +x /usr/local/bin/kubectl
-echo "Waiting for K8S to initialize.."
-sleep 30
-kubectl get po
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl get po --all-namespaces
