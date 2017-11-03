9c09a2947e5c7d79b2fcba3aa2c3bc3a32b6a576
Yutong Zhang <yutongz@google.com>
2017-11-02 21:52:53 -0700

    change sybolic link

--- prow/pilot-coverage.sh
+++ prow/pilot-coverage.sh
@@ -29,8 +29,8 @@ ln -sf "${HOME}/.kube/config" platform/kube/config
 if [ "${CI:-}" == 'bootstrap' ]; then
     # Test harness will checkout code to directory $GOPATH/src/github.com/istio/istio
     # but we depend on being at path $GOPATH/src/istio.io/istio for imports.
-    ln -sf ${GOPATH}/src/github.com/istio ${GOPATH}/src/istio.io
-    cd ${GOPATH}/src/istio.io/old_pilot_repo
+    ln -sf ${GOPATH}/src/github.com/istio/old_pilot_repo ${GOPATH}/src/istio.io/pilot
+    cd ${GOPATH}/src/istio.io/pilot
 fi
 
 echo '=== Init ==='
